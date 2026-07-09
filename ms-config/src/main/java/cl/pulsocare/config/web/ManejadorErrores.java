639cce1ec6d6e8188bc9f73537b1848c436fd98c
Diego Andrés Muñoz Morales <86175879+DimmoX@users.noreply.github.com>
2026-07-08 21:41:39 -0400

    feat: agregar api rest de umbrales

---
+++ ms-config/src/main/java/cl/pulsocare/config/web/ManejadorErrores.java
@@ -0,0 +1,22 @@
+package cl.pulsocare.config.web;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/** Traduce errores tecnicos a respuestas HTTP claras. */
+@RestControllerAdvice
+public class ManejadorErrores {
+
+    /** Ej.: crear un umbral con un idPaciente o idSignoVital inexistente (FK). */
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ProblemDetail integridad(DataIntegrityViolationException e) {
+        ProblemDetail pd = ProblemDetail.forStatus(HttpStatus.CONFLICT);
+        pd.setTitle("Conflicto de integridad");
+        pd.setDetail("La operacion viola una restriccion de la base de datos " +
+                "(por ejemplo, el paciente o el signo vital indicado no existe).");
+        return pd;
+    }
+}
